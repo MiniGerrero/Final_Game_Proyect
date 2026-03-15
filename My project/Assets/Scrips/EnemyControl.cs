b19f1edc017146508fc81041ed6b82edbc8d7f9b
MiniGerrero <bellseb000@alpinesd.org>
2026-03-14 18:25:36 -0600

    Enemy .01V
    Are just some base for Enemy Sistem

--- My project/Assets/Scrips/EnemyControl.cs
+++ My project/Assets/Scrips/EnemyControl.cs
@@ -2,15 +2,22 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Referense")]
+    [SerializeField] private Rigidbody2D rigy;
+    [SerializeField] private EnemyState enemyState;
+
+    [Header("Movement")]
+    [SerializeField] private float baseVelocity;
+    [SerializeField] private float actualVelocity;
+
+
+    // Update is called once per frame
+    private void Update()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+
     }
 }
---
+++ My project/Assets/Scrips/EnemyState.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public enum EnemyState 
+{
+    Patrol,
+    Attack,
+    Waiting,
+}
+
