105468a472f972402c463427ae8030bfe01f0b90
MiniGerrero <bellseb000@alpinesd.org>
2026-03-25 12:16:51 -0600

    SomeMore Funtion

--- My project/Assets/Platform/Scrips/EnemyControl.cs
+++ My project/Assets/Platform/Scrips/EnemyControl.cs
@@ -13,8 +13,10 @@ public class Enemy : MonoBehaviour
     [Header("Obtacule Detection")]
     [SerializeField] private Transform controlFront;
     [SerializeField] private LayerMask wallMask;
+    [Tooltip("You can add more Layers if You want")]
     [SerializeField] private bool wallTouched;
     [SerializeField] private float distanceLineDetection;
+    private int collisionMask;
 
     [Header("Damages")]
     public float damage;
@@ -24,35 +26,34 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     private void Start(){
         actualVelocity = baseVelocity;
+        
     }
 
-    private void Update()
-    {
-        wallTouched = Physics2D.Raycast(controlFront.position, transform.right, distanceLineDetection, wallMask);
+    private void Update(){
+        wallTouched = Physics2D.Raycast(controlFront.position, transform.right, distanceLineDetection, wallMask );
     }
 
-    private void FixedUpdate()
-    {
+    private void FixedUpdate(){
         switch (enemyState)
         {
             case EnemyState.Patrol:
                 ChangeDirecion();
                 break;
+            case EnemyState.Attack:
+
             default:
                 break;
         }
         rigy.linearVelocity = new Vector2(actualVelocity, rigy.linearVelocity.y);
     }
 
-    private void OnDrawGizmos()
-    {
+    private void OnDrawGizmos(){
         Gizmos.color = Color.red;
         Gizmos.DrawLine(controlFront.position, controlFront.position + distanceLineDetection * transform.right);
        
     }
-    
-    private void ChangeDirecion()
-    {
+    //Patrol Code
+    private void ChangeDirecion(){
         if (wallTouched)
         {
             actualVelocity *= -1;
@@ -61,5 +62,9 @@ public class Enemy : MonoBehaviour
             transform.eulerAngles = new Vector3(0, rotation, 0);
 
         }
+    }
+    //Attack Code
+    private void PlayerFollow(){
+
     }
 }
